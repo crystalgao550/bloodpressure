963f34424eb2bf96ee6776068092648a725a9eac
x00193215 <x00193215@mytudublin.ie>
2023-05-04 10:54:09 +0100

    upload to github

---
+++ cicd-EAD/BE/src/main/java/bp/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package bp;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ */
+@ApplicationPath("retrieve")
+public class ApplicationConfig extends Application {
+    
+}
